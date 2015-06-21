9aa102a2b4882c94f3d9ded6f1aa20f00bd2d5fa
ma7mood <sa3d.ma7mood@gmail.com>
2015-06-21 12:08:13 +0800

    file contains the two functions that create special matrix object to store the matri and cache its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are used to create a 'special' matrix object
+## that stores a matrix and caches its mean.
 
-## Write a short comment describing this function
+## creates a special matrix object, essentially an 'interface'(list of 4 functions), 
+## to get and set the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # initialise the inverse to NULL
+    inverseOfx <- NULL
+    
+    # set the value of m to the new value and thus reset inverseOfm to NULL
+    set <- function(newValue) {
+        x <<- newValue
+        inverseOfx <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) inverseOfx <<- inv
+    getInverse <- function() inverseOfx
+    
+    #returns the list of the 4 functions, our 'special' matrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## produces the inverse of the 'special' matrix
+## by retrieving it from the cache if present otherwise calculating it
+## and setting the inverse in the cache via setInverse function
+## the '...' are arguments (if any) to be passed to the internal 'solve' function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    inverseOfmatrix <- x$getInverse()
+
+    # if inverse is cached then return its value
+    if (!is.null(inverseOfmatrix)) {
+        message("getting cached data")
+        return(inverseOfmatrix)
+    }
+    
+    # if inverse not cached then:
+    # 1. calculate it
+    matrixData <- x$get()
+    inverseOfmatrix <- solve(matrixData, ...)
+    # 2. attach it to the matrix
+    x$setInverse(inverseOfmatrix)
+    # 3. return its value
+    inverseOfmatrix
 }
